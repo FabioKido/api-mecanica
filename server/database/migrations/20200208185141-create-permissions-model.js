697372b234c39d0e7441e33e1b528678973a5475
FabioKido <fabiohenryquemesquita@gmail.com>
2020-02-08 15:55:39 -0300

    Add: Migrations de Grupos e Permissões

---
+++ server/database/migrations/20200208185141-create-permissions-model.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('permissions', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      action: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      enable: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_by: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      updated_by: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('permissions');
+  }
+};
