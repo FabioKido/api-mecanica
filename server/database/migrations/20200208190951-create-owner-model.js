24ce8918267a06dc1af0a3821444c17d5239b57b
FabioKido <fabiohenryquemesquita@gmail.com>
2020-02-08 16:28:09 -0300

    Add: Migrations de Owner/Worker/Company - Type Users

---
+++ server/database/migrations/20200208190951-create-owner-model.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('owner', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sex: {
+        type: Sequelize.STRING(1),
+        allowNull: true,
+      },
+      cpf: {
+        type: Sequelize.STRING(14),
+        allowNull: true,
+      },
+      rg: {
+        type: Sequelize.STRING(14),
+        allowNull: true,
+      },
+      birthday: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      orgao_expeditor: {
+        type: Sequelize.STRING(30),
+        allowNull: true,
+      },
+      id_contact: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('owner');
+  }
+};
---
+++ server/database/migrations/20200208192036-create-company-model.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('company', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      nome_fantasia: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cnpj: {
+        type: Sequelize.STRING(25),
+        allowNull: true,
+      },
+      ie: {
+        type: Sequelize.STRING(13),
+        allowNull: true,
+      },
+      id_contact: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('company');
+  }
+};
