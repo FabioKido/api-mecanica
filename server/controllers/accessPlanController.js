f2f0f1620ead8b7c8f09ca4b1d00667f25270292
FabioKido <fabiohenryquemesquita@gmail.com>
2020-02-13 00:21:08 -0300

    Atualizados - Controller de Planos de Acesso / Rotas

--- server/controllers/accessPlanController.js
+++ server/controllers/accessPlanController.js
@@ -1,6 +1,26 @@
 const AccessPlan = require('../models/AccessPlan');
 const Resource = require('../models/Resource');
 
+exports.getAccessPlans = async (req, res, next) => {
+  const accessPlans = await AccessPlan.findAll();
+  res.status(200).json({
+    data: accessPlans
+  });
+}
+
+exports.getAccessPlan = async (req, res, next) => {
+  try {
+    const planId = req.params.planId;
+    const accessPlan = await AccessPlan.findByPk(planId);
+    if (!accessPlan) return next(new Error('Plano de Acesso não existe'));
+    res.status(200).json({
+      data: accessPlan
+    });
+  } catch (error) {
+    next(error)
+  }
+}
+
 exports.addAccessPlan = async (req, res, next) => {
   try {
     const { name, type, value, enable, created_by, updated_by } = req.body;
@@ -19,6 +39,75 @@ exports.addAccessPlan = async (req, res, next) => {
       message: "Plano de Acesso cadastrado com sucesso"
     })
 
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.updateAccessPlan = async (req, res, next) => {
+  try {
+
+    const planId = req.params.planId;
+    const { name, type, value, enable } = req.body
+    
+    const accessPlan = await AccessPlan.update( { 
+      name,
+      type,
+      value,
+      enable,
+     },
+     { 
+      where: {
+        id: planId
+      } 
+    });
+
+    res.json({
+      data: accessPlan,
+      message: "Plano de Acesso atualizado com sucesso"
+    })
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.deleteAccessPlan = async (req, res, next) => {
+  try {
+    
+    const planId = req.params.planId;
+
+    AccessPlan.destroy({
+      where: {
+        id: planId
+      }
+    })
+
+    res.status(200).json({
+      data: null,
+      message: 'Plano de Acesso foi deletado'
+    });
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.getResources = async (req, res, next) => {
+  const resource = await Resource.findAll();
+  res.status(200).json({
+    data: resource
+  });
+}
+
+exports.getResource = async (req, res, next) => {
+  try {
+    const resourceId = req.params.resourceId;
+    const resource = await Resource.findByPk(resourceId);
+    if (!resource) return next(new Error('Recurso não existe'));
+    res.status(200).json({
+      data: resource
+    });
   } catch (error) {
     next(error)
   }
@@ -48,6 +137,55 @@ exports.addResource = async (req, res, next) => {
       data: resource,
       message: "Recurso cadastrado com sucesso"
     })
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.updateResource = async (req, res, next) => {
+  try {
+
+    const resourceId = req.params.resourceId;
+    const { name, action, enable, qtd } = req.body;
+    
+    const resource = await Resource.update( { 
+      name,
+      action,
+      enable,
+      qtd,
+     },
+     { 
+      where: {
+        id: resourceId
+      } 
+    });
+
+    res.json({
+      data: resource,
+      message: "Recurso atualizado com sucesso"
+    })
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.deleteResource = async (req, res, next) => {
+  try {
+
+    const resourceId = req.params.resourceId;
+
+    Resource.destroy({
+      where: {
+        id: resourceId
+      }
+    })
+
+    res.status(200).json({
+      data: null,
+      message: 'Recurso foi deletado'
+    });
 
   } catch (error) {
     next(error)
--- server/routes/route.js
+++ server/routes/route.js
@@ -8,6 +8,7 @@ const accessPlanController = require('../controllers/accessPlanController');
 const contactController = require('../controllers/contactController');
 const groupController = require('../controllers/groupController');
 
+//User and Controll
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.get('/user/:userId', userController.allowIfLoggedin, userController.getUser);
@@ -15,24 +16,35 @@ router.get('/users', userController.allowIfLoggedin, userController.grantAccess(
 router.put('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
 router.delete('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
 
+//Group and Permission
 router.post('/add-group', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), groupController.createGroup);
-//router.get('/group/:groupId', userController.allowIfLoggedin, groupController.getGroup);
+router.get('/group/:groupId', userController.allowIfLoggedin, groupController.getGroup);
 router.get('/groups', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), groupController.getGroups);
+//router.post('/add-permission', userController.allowIfLoggedin, groupController.addPermission);
 //router.get('/permission/:permissionId', userController.allowIfLoggedin, groupController.getPermission);
-//router.post('/add-permission', groupController.addPermission);
 
+//User and Group
 router.post('/users/:id_user/add-group', userController.allowIfLoggedin, groupController.addUserInGroup);
-router.delete('/users/:id_user/del-group', userController.allowIfLoggedin, groupController.deleteUserInGroup);
 router.get('/users/:id_user/group', userController.allowIfLoggedin, groupController.getUserInGroup);
+router.delete('/users/:id_user/del-group', userController.allowIfLoggedin, groupController.deleteUserInGroup);
 
+//User and Contact
 router.post('/user/:id_user/owner', userController.allowIfLoggedin, ownerController.addOwner);
 router.post('/user/:id_user/worker', userController.allowIfLoggedin, workerController.addWorker);
 router.post('/user/:id_user/company', userController.allowIfLoggedin, companyController.addCompany);
 router.get('/worker/:id_worker/contact', userController.allowIfLoggedin, workerController.getWorkerContact);
-
 router.post('/user/new-contact', userController.allowIfLoggedin, contactController.addContact);
 
+//Access Plan and Resources
 router.post('/plans/new-plan', userController.allowIfLoggedin, accessPlanController.addAccessPlan);
+router.get('/plan/:planId', userController.allowIfLoggedin, accessPlanController.getAccessPlan);
+router.get('/plans', userController.allowIfLoggedin, accessPlanController.getAccessPlans);
+router.put('/plan/:planId/update-plan', userController.allowIfLoggedin, accessPlanController.updateAccessPlan);
+router.delete('/plan/:planId/del-plan', userController.allowIfLoggedin, accessPlanController.deleteAccessPlan);
 router.post('/plans/:id_access_plan/new-resource', userController.allowIfLoggedin, accessPlanController.addResource);
+router.get('/resource/:resourceId', userController.allowIfLoggedin, accessPlanController.getResource);
+router.get('/resources', userController.allowIfLoggedin, accessPlanController.getResources);
+router.put('/resource/:resourceId/update-resource', userController.allowIfLoggedin, accessPlanController.updateResource);
+router.delete('/resource/:resourceId/del-resource', userController.allowIfLoggedin, accessPlanController.deleteResource);
 
 module.exports = router;
